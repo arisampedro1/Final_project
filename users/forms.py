ab3b74a1cd6b33ee6b68f5d6750e6c9c318962c5
Ariana Sampedro <SAMPEDROARI@GMAIL.COM>
2024-08-12 23:04:51 -0300

    cambios registro

--- users/forms.py
+++ users/forms.py
@@ -3,20 +3,14 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repetir contraseña', widget=forms.PasswordInput)
-
+class UserCreationForm(BaseUserCreationForm):
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
-        help_texts = {k: "" for k in fields}
-
+        fields = ['username', 'password1', 'password2']
+    
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.is_superuser = True
-        user.is_staff = True 
+        user.is_superuser = True  
         if commit:
             user.save()
         return user
